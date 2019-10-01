daa7948574973ddb65efae1daef4a7e928d648ed
Yopciem <maciekkopec007@gmail.com>
2019-10-01 19:07:15 +0200

    fuze function

--- gravity/gravity1.py
+++ gravity/gravity1.py
@@ -16,15 +16,14 @@ angleSpeed = 0
 
 vel = Vector2()
 dPosition = Vector2()
-
+stars=[]
 win = pygame.display.set_mode(WIN_SIZE)
 pygame.init()
 
 class Body(object):
     def __init__(self, win, x, y, mass, st = True):
         self.win = win
-        self.x = x
-        self.y = y
+        self.pos = Vector2(x,y)
         self.mass = mass
         self.isStatic = st
 
@@ -36,20 +35,20 @@ class Planet(Body):
         self.color = color
 
     def Draw(self):
-        pygame.draw.circle(self.win, self.color, (int(self.x), int(self.y)), int(self.radius))
+        pygame.draw.circle(self.win, self.color, (int(self.pos.x), int(self.pos.y)), int(self.radius))
 
 
 class Orb(Planet):
 
     def __init__(self, win, x, y, mass, radius, color = (255,255,255), dispayVectors=False,st=False):
         super().__init__(win, x, y, mass, radius, color,st)
-        self.vel = Vector2()
-        self.p_vel = Vector2()
+        self.vel = Vector2(0,0)
+        self.p_vel = Vector2(0,0)
         self.dVec = dispayVectors
 
     def Draw(self): #drawing ball
         super().Draw()
-        pygame.draw.circle(self.win, (0, 100, 0), (int(self.x), int(self.y)), self.radius-2)
+        pygame.draw.circle(self.win, (0, 100, 0), (int(self.pos.x), int(self.pos.y)), int(self.radius-2))
         if self.dVec:
             if DRAW_ACC_VEC:
                 self.drawacc()
@@ -58,22 +57,26 @@ class Orb(Planet):
                 self.drawspeed()
 
     def drawacc(self): #drawing accelerate
-        pygame.draw.line(self.win, (255, 0, 0),(self.x, self.y), (self.x+(self.vel.x-self.p_vel.x)*ACC_VECTOR_SCALE, self.y+(self.vel.y-self.p_vel.y)*ACC_VECTOR_SCALE), 2)
-
+        pygame.draw.line(self.win, (255, 0, 0),self.pos, (self.vel-self.p_vel)*ACC_VECTOR_SCALE + self.pos, 2)
     def drawspeed(self): #drawing velocity
-        pygame.draw.line(self.win, (50, 50, 255),(self.x,self.y), (self.vel.x*SPEED_VECTOR_SCALE+self.x, self.vel.y*SPEED_VECTOR_SCALE+self.y), 2)
+        pygame.draw.line(self.win, (50, 50, 255),self.pos, self.vel * SPEED_VECTOR_SCALE + self.pos, 2)
 
     def movement(self): #ball movement
-        self.x += self.vel.x
-        self.y += self.vel.y
+        self.pos += self.vel/self.mass*VEL_SCALE
+
+    def Fuze(self, obj):
+        self.vel = (self.vel*self.mass + obj.vel*obj.mass)/(self.mass + obj.mass)
+        self.mass += obj.mass
+        self.pos += (obj.pos - self.pos)/(1+((self.radius/obj.radius)**2))
+        self.radius = math.sqrt(self.radius**2+obj.radius**2)
 
 def redraw(): #TODO list of elements to draw
     win.fill((0, 0, 0))
-    orb.Draw()
-    ball.Draw()
+    for obj in stars:
+        obj.Draw()
     
 def line(pos): #drawing line
-    pygame.draw.line(win, (255, 255, 255), [ball.x, ball.y], pos, 3)
+    pygame.draw.line(win, (255, 255, 255), [ball.pos.x, ball.pos.y], pos, 3)
 
 def DeltaVector(pos1,pos2,scale):
     vec = Vector2(0,0)
@@ -84,22 +87,16 @@ def DeltaVector(pos1,pos2,scale):
         Sum = scale / distance**2
         vec.x = Sum*distanceX/distance
         vec.y = Sum*distanceY/distance
-        print(vec.x,vec.y)
-        return vec
+    return vec
+    
 
 def Gravity(object1,object2): #creating gravity force
-    acc = DeltaVector(Vector2(object1.x,object1.y),Vector2(object2.x,object2.y),G * object1.mass * object2.mass)
+    acc = DeltaVector(object1.pos,object2.pos,G * object1.mass * object2.mass)
     if not object1.isStatic:
-        object1.vel.x += acc.x
-        object1.vel.y += acc.y
-        object1.movement()
+        object1.vel += acc
     if not object2.isStatic:
-        object2.vel.x += -acc.x
-        object2.vel.y += -acc.y
-        object2.movement()
+        object2.vel -= acc
 
-orb = Orb(win,800,400,ORB_MASS,ORB_SIZE,ORB_COLOR,True)
-orb.Draw()
 
 while True:
     delta+= clock.tick()/1000.0
@@ -115,10 +112,11 @@ while True:
             if event.type == pygame.MOUSEBUTTONDOWN:
                 start_pos = m_pos
                 ball = Orb(win, m_pos.x, m_pos.y, ORB_MASS, ORB_SIZE,ORB_COLOR, True)
+                stars.append(ball)
                 click = True
                 space = False
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE and space == False:
-                ball.vel = Vector2(m_pos.x-ball.x,m_pos.y-ball.y)*POWER_SCALE
+                ball.vel = Vector2(m_pos.x-ball.pos.x,m_pos.y-ball.pos.y)*POWER_SCALE/ball.mass
                 space = True
 
         if click:
@@ -126,7 +124,19 @@ while True:
             if not space:
                 line(m_pos)
             else:
-                Gravity(ball,orb)
+                for nr1 in range(len(stars)):
+                    for nr2 in range(nr1+1,len(stars)):
+                        Gravity(stars[nr1],stars[nr2])
+                for obj in stars:
+                    obj.movement()
+
+                for object1 in stars:
+                    for object2 in stars:
+                        if object1 == object2:
+                            continue
+                        elif object1.pos.distance_to(object2.pos) < max(object1.radius,object2.radius):
+                            object1.Fuze(object2)
+                            stars.remove(object2)
 
 
 
